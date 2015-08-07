2e054028834ad35234cd931aedb5a5ab1c4a2e9b
Hengyang Zhao <hzhao@ece.ucr.edu>
2015-08-07 16:48:54 -0700

    Remove track of ~/.bash_profile

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PROJ_DIR=`pwd`
 cd ~
 for f in $PROJ_DIR/dot_*; do
     f=`basename "$f"`
-    echo $PROJ_DIR/$f '->' .${f##dot_}
-    ln -sf $PROJ_DIR/$f .${f##dot_}
+    echo $PROJ_DIR/$f '->' .${f#dot_}
+    ln -sf $PROJ_DIR/$f .${f#dot_}
 done
 
