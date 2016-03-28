691c8fa134820c3ebe26fb4d1e2a34d7a5558674
Hengyang Zhao <hzhao@ece.ucr.edu>
2016-03-28 13:22:05 -0700

    Now install.sh can interactively generate gitconfig

--- install.sh
+++ install.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
 PROJ_DIR=`pwd`
+ENV_DIR=~/.site_env
+
+echo Creating symlinks for resource files:
 
 cd ~
 for f in $PROJ_DIR/dot_*; do
     f=`basename "$f"`
-    echo $PROJ_DIR/$f '->' .${f#dot_}
+    echo ~/.${f#dot_} '->' $PROJ_DIR/$f
     ln -sf $PROJ_DIR/$f .${f#dot_}
 done
 
+mkdir -p $ENV_DIR
+
+echo
+echo "Collecting user information for git configuration file:"
+
+echo -n "Please enter your full name: "
+read NAME
+
+echo -n "Please enter your main E-mail address: "
+read EMAIL
+
+cat > $ENV_DIR/gitconfig << EOF
+[user]
+    name = $NAME
+    email = $EMAIL
+EOF
+
+echo "Generated file $ENV_DIR/gitconfig"
+
+echo
+echo "<> <> <>  W E L C O M E   H O M E  <> <> <>"
+echo
