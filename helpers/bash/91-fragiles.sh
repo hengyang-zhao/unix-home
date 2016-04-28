565e4756eea587e396a5509a1a6a012b199e9db0
Hengyang Zhao <hzhao@ece.ucr.edu>
2016-04-28 10:33:33 -0700

    Alias make to __smart_make

--- helpers/bash/91-fragiles.sh
+++ helpers/bash/91-fragiles.sh
@@ -5,7 +5,37 @@ __red_stderr()
 
 __verbose_do()
 {
-	echo -e "\e[1m-> $@\e[0m"
+	echo -e "\e[1m=> $@\e[0m"
 	eval "$@"
 	return $?
+}
+
+__highlighted_echo()
+{
+	echo -e "\e[1m$@\e[0m"
+}
+
+__red_echo()
+{
+	echo -e "\e[31m$@\e[0m"
+}
+
+__has make && alias make=__smart_make
+__smart_make()
+{
+	dir=.
+	while true; do
+		echo "** Attempting make in $(realpath $dir)"
+		if [ -f $dir/Makefile ] || [ -f $dir/makefile ] || [ -f $dir/GNUmakefile ]; then
+			$(which --skip-alias make) -C "$dir" $@
+			return $?
+		fi
+
+		if [ "$(realpath "$dir")" = / ]; then
+			__red_echo "** Cannot find makefile"
+			return 1
+		fi
+
+		dir+=/..
+	done
 }
