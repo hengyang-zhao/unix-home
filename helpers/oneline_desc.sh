6bf11303167ef5443591595480bb74a9f9a6faec
Hengyang Zhao <hengyang@synopsys.com>
2016-07-08 16:48:31 -0700

    Add mem free % in one-line description script

--- helpers/oneline_desc.sh
+++ helpers/oneline_desc.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 KERNEL=`uname -s`
-FORMAT="%-12s%-18s%-10s%-8s%-8s\n"
+FORMAT="%-12s%-18s%-10s%-7s%-6s%-8s\n"
 
 if [ "$1" = "--header" ]; then
-    printf "$FORMAT" "Host" "CPU" "Mem (GB)" "Load" "Screens"
+    printf "$FORMAT" "Host" "CPU" "Mem (GB)" "%Free" "Load" "Screens"
     exit
 fi
 
-HOST=$1
+HOST=${1:-localhost}
 
 case $KERNEL in
     Linux)
@@ -19,10 +19,11 @@ case $KERNEL in
         memTotalGB=`expr $memTotalKB / 1024 / 1024`
         memFreeKB=`grep MemFree /proc/meminfo | sed 's/MemFree:\s*\([0-9]\+\) kB/\1/g'`
         memFreeGB=`expr $memFreeKB / 1024 / 1024`
+        memFreePct=`expr $memFreeKB \* 100 / $memTotalKB`%
         loadAvg1Min=`sed 's/^\([0-9\.]\+\) .*$/\1/g' /proc/loadavg`
-        screens=`screen -ls | grep -E '[0-9]\.' | wc -l`
+        gnuScreenCnt=`screen -ls | grep -E '[0-9]\.' | wc -l`
 
-        printf "$FORMAT" $HOST "$cpuFreq x $cpuCores" "$memFreeGB/$memTotalGB" $loadAvg1Min $screens
+        printf "$FORMAT" $HOST "$cpuFreq x $cpuCores" "$memFreeGB/$memTotalGB" $memFreePct $loadAvg1Min $gnuScreenCnt
         ;;
 
     *)
