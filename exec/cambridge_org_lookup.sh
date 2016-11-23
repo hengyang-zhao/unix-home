648c7bdc9102c994ec935b3d4abdb075793e9355
Hengyang Zhao <hzhao@ece.ucr.edu>
2016-11-22 17:39:04 -0800

    Remove obsolete files under exec/

--- exec/cambridge_org_lookup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-word=$(echo $(xsel) | xargs | sed -e 's/ /-/g' | tr '[:upper:]' '[:lower:]')
-urlhead='http://dictionary.cambridge.org/us/dictionary/english-chinese-traditional'
-exec xdg-open "$urlhead/$word" &>/dev/null
-
-# vim: set ft=sh:
-
