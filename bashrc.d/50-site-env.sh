0024ab2bde8d6f5ea77283ff8569b5c35e2c2bc6
Hengyang Zhao <hzhao@877502@gmail.com>
2016-11-26 12:04:25 -0800

    Fix bug in bashrc.d/50-site-env.sh

--- bashrc.d/50-site-env.sh
+++ bashrc.d/50-site-env.sh
@@ -1,14 +1,19 @@
 # Site scripts
-if [ -d ~/.site_env/bash ]; then
-    local IFS=$' \t\n'
-	for i in ~/.site_env/bash/*.sh ; do
-		if [ -r "$i" ]; then
-			if [ "${-#*i}" != "$-" ]; then
-				. "$i"
-			else
-				. "$i" >/dev/null 2>&1
-			fi
-		fi
-	done
-fi
+
+function __source_site_rc() {
+    if [ -d ~/.site_env/bash ]; then
+        local IFS=$' \t\n'
+        for i in ~/.site_env/bash/*.sh ; do
+            if [ -r "$i" ]; then
+                if [ "${-#*i}" != "$-" ]; then
+                    . "$i"
+                else
+                    . "$i" >/dev/null 2>&1
+                fi
+            fi
+        done
+    fi
+}
+
+__source_site_rc
 
